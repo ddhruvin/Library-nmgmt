228b8c50dd7aef40674b2ce795be58fe3258eb3b
Dhruvin <120237476+ddhruvin@users.noreply.github.com>
2024-03-17 10:51:13 +0530

    Add files via upload

---
+++ Library_Managemant_System.java
@@ -0,0 +1,32 @@
+/* * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package library_managemant_system;
+
+/**
+ *
+ * @author dhruv
+ */
+public class Library_Managemant_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new SignupPage().setVisible(true);
+    }
+
+
+    private static class SignupPage {
+
+        public SignupPage() {
+        }
+
+        private void setVisible(boolean b) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+    }
+    
+}
